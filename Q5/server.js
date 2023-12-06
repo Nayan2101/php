d479d53f9f317f1cfc3b8cdf908bdedbc5de8d65
nayan <nayan234.rejoice@gmail.com>
2023-12-06 09:41:00 +0530

    update

--- Q5/server.js
+++ Q5/server.js
@@ -5,6 +5,7 @@ const body_parser = require("body-parser");
 const cors = require("cors");
 const userRoutes = require("./Apis/routes/userRoutes");
 const authRoutes = require("./Apis/routes/authRoutes");
+const ejs = require("ejs");
 
 mongoose
   .connect("mongodb://localhost:27017/exam", {
@@ -21,6 +22,9 @@ app.use(express.json());
 app.use(body_parser.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use("/", express.static("./"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/api/", userRoutes);
 app.use("/api/", authRoutes);
